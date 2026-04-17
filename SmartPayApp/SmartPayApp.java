d9cfbfc3b86f86b07c491ce4ca9e552d0d87e3f2
pavithra8994 <132538033+pavithra8994@users.noreply.github.com>
2026-04-17 13:49:52 +0530

    Add files via upload

---
+++ SmartPayApp/SmartPayApp.java
@@ -0,0 +1,97 @@
+import java.util.Scanner;
+
+// Interface
+interface Billable {
+    double calculateTotal();
+}
+
+// Class implementing interface
+class UtilityBill implements Billable {
+
+    String customerName;
+    int prevReading;
+    int currReading;
+    int units;
+    double totalAmount;
+
+    // Constructor
+    UtilityBill(String name, int prev, int curr) {
+        this.customerName = name;
+        this.prevReading = prev;
+        this.currReading = curr;
+    }
+
+    // Method to calculate bill
+    public double calculateTotal() {
+
+        units = currReading - prevReading;
+        double amount = 0;
+
+        
+        if (units <= 100) {
+            amount = units * 1.0;
+        } else if (units <= 300) {
+            amount = (100 * 1.0) + ((units - 100) * 2.0);
+        } else {
+            amount = (100 * 1.0) + (200 * 2.0) + ((units - 300) * 5.0);
+        }
+
+        // Tax calculation (10%)
+        double tax = amount * 0.10;
+        totalAmount = amount + tax;
+
+        return totalAmount;
+    }
+
+    // Method to print receipt
+    void print() {
+        System.out.println("\n===== SMARTPAY RECEIPT =====");
+        System.out.println("Customer Name : " + customerName);
+        System.out.println("Units Consumed: " + units);
+        System.out.println("Total Amount  : $" + totalAmount);
+        System.out.println("============================");
+    }
+}
+
+// Main class
+public class SmartPayApp {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.print("\nEnter Customer Name (or type Exit): ");
+            String name = sc.nextLine();
+
+            // Exit condition
+            if (name.equalsIgnoreCase("Exit")) {
+                System.out.println("Exiting Application...");
+                break;
+            }
+
+            System.out.print("Enter Previous Reading: ");
+            int prev = sc.nextInt();
+
+            System.out.print("Enter Current Reading: ");
+            int curr = sc.nextInt();
+            sc.nextLine(); 
+
+            // Validation
+            if (prev > curr) {
+                System.out.println("Error: Previous reading cannot be greater than current reading!");
+                continue;
+            }
+
+            // Object creation
+            UtilityBill bill = new UtilityBill(name, prev, curr);
+
+            // Method calls
+            bill.calculateTotal();
+            bill.print();
+        }
+
+        sc.close();
+    }
+}
