83143e15a959aa319a61341f2dccb5ff8ff60d6e
pavithra8994 <132538033+pavithra8994@users.noreply.github.com>
2026-04-22 21:57:58 +0530

    Update FareCalc.py

--- Python usecase/FareCalc.py
+++ Python usecase/FareCalc.py
@@ -1,23 +1,20 @@
-# FareCalc - Travel Optimizer
-
-# Step 1: Dictionary for vehicle rates
 rates = {
     "Economy": 10,
     "Premium": 18,
-    "SUV": 25
+    "Suv": 25
 }
 
-# Step 3: Function to calculate fare
+
 def calculate_fare(km, vehicle_type, hour):
     
-    # Step 4: Error Handling
+    
     if vehicle_type not in rates:
         return "Service Not Available"
     
     base_rate = rates[vehicle_type]
     total = km * base_rate
     
-    # Step 2: Surge Pricing Logic
+    
     if 17 <= hour <= 20:
         total *= 1.5
         surge_applied = True
@@ -27,24 +24,25 @@ def calculate_fare(km, vehicle_type, hour):
     return total, surge_applied
 
 
-# -------- MAIN PROGRAM --------
+
 try:
-    print("\n===== CityCab Ride Estimate =====")
+    print("\n-------- CityCab Ride Estimate ---------")
     
     km = float(input("Enter distance (in km): "))
-    vehicle_type = input("Enter vehicle type (Economy/Premium/SUV): ")
+    vehicle_type = input("Enter vehicle type (Economy/Premium/Suv): ")
+    
     hour = int(input("Enter hour of travel (0-23): "))
     
     result = calculate_fare(km, vehicle_type, hour)
     
-    # If invalid vehicle type
+    
     if result == "Service Not Available":
         print("\n Service Not Available for selected vehicle type.")
     
     else:
         total, surge = result
         
-        print("\n===== PRICE RECEIPT =====")
+        print("\n------ PRICE RECEIPT ------")
         print(f"Distance       : {km} km")
         print(f"Vehicle Type   : {vehicle_type}")
         print(f"Base Rate/km   : ₹{rates[vehicle_type]}")
@@ -55,7 +53,7 @@ try:
             print("Surge Applied  : No")
         
         print(f"Total Fare     : ₹{total:.2f}")
-        print("=============================")
+        print("-------------------------")
 
 except ValueError:
     print("\n Invalid input! Please enter correct numeric values.")
