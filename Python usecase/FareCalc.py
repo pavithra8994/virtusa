7c08692f577a57a7fb6b9a6c3e0e6d4366b90a5c
pavithra8994 <132538033+pavithra8994@users.noreply.github.com>
2026-05-14 22:57:31 +0530

    Update FareCalc.py

--- Python usecase/FareCalc.py
+++ Python usecase/FareCalc.py
@@ -5,55 +5,79 @@ rates = {
 }
 
 
+
 def calculate_fare(km, vehicle_type, hour):
-    
+
     
     if vehicle_type not in rates:
         return "Service Not Available"
+
+    
     
     base_rate = rates[vehicle_type]
     total = km * base_rate
-    
+
     
     if 17 <= hour <= 20:
         total *= 1.5
         surge_applied = True
     else:
         surge_applied = False
-    
+
     return total, surge_applied
 
 
 
 try:
-    print("\n-------- CityCab Ride Estimate ---------")
-    
+    print("-------- CityCab Ride Estimate ---------")
+
+   
     km = float(input("Enter distance (in km): "))
     vehicle_type = input("Enter vehicle type (Economy/Premium/Suv): ")
-    
     hour = int(input("Enter hour of travel (0-23): "))
+
     
-    result = calculate_fare(km, vehicle_type, hour)
+    if km < 0:
+        print("Invalid distance! Distance cannot be negative.")
+
     
-    
-    if result == "Service Not Available":
-        print("\n Service Not Available for selected vehicle type.")
-    
+    elif hour < 0 or hour > 23:
+        print("Invalid hour! Please enter between 0 and 23.")
+
     else:
-        total, surge = result
         
-        print("\n------ PRICE RECEIPT ------")
-        print(f"Distance       : {km} km")
-        print(f"Vehicle Type   : {vehicle_type}")
-        print(f"Base Rate/km   : ₹{rates[vehicle_type]}")
+        result = calculate_fare(km, vehicle_type, hour)
+
         
-        if surge:
-            print("Surge Applied  : Yes (1.5x)")
+        if result == "Service Not Available":
+            print("Service Not Available for selected vehicle type.")
+
+        
+        elif result == "Invalid distance":
+            print("Invalid distance! Distance cannot be negative.")
+
         else:
-            print("Surge Applied  : No")
-        
-        print(f"Total Fare     : ₹{total:.2f}")
-        print("-------------------------")
+            total, surge = result
+
+            
+            print("------ PRICE RECEIPT ------")
+            print("Distance       :", km, "km")
+            print("Vehicle Type   :", vehicle_type)
+            print("Base Rate/km   : ₹", rates[vehicle_type])
+
+            if surge:
+                print("Surge Applied  : Yes (1.5x)")
+            else:
+                print("Surge Applied  : No")
+
+            print("Total Fare     : ₹{:.2f}".format(total))
+            print("---------------------------")
+
+
 
 except ValueError:
-    print("\n Invalid input! Please enter correct numeric values.")
+    print("Invalid input! Please enter correct numeric values.")
+
+
+
+
